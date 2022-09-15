8867bd29e3415f560d76a631ae67aa380e2c47b6
gregorywyrick <gregmwyrick@gmail.com>
2022-09-15 16:19:57 -0500

    building on js and tweaks to css for preference

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 var currentHr = parseInt(moment().format('H'));
 var classes = [".8AM", ".9AM", ".10AM", ".11AM", ".12PM", ".1PM", ".2PM", ".3PM", ".4PM", ".5PM", ".6PM"]
 var time = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+var allNotes = ["", "", "", "", "", "", "", "", "", "", ""]
+var classIndex = time.indexOf(hourNow);
+$("#currentDay").text(moment().format('dddd, MMMM D, YYYY'));
+var currentDayCheck = moment().format('dddd, MMMM D, YYYY')
+
+if (currentHr < 9) {
+    allFuture();
+}
+else if (currentHr > 17) {
+    allPast();
+}
+else {
+    formatTimes()
+}
