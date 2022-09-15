ca5e9ed13d0983f478188922474c4f8ec70f98bc
gregorywyrick <gregmwyrick@gmail.com>
2022-09-15 16:14:00 -0500

    js file and variables added

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+var currentHr = parseInt(moment().format('H'));
+var classes = [".8AM", ".9AM", ".10AM", ".11AM", ".12PM", ".1PM", ".2PM", ".3PM", ".4PM", ".5PM", ".6PM"]
+var time = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
