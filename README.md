5583e6716744be7d6f434c512158d82081d7539b
gregorywyrick <gregmwyrick@gmail.com>
2022-09-15 16:27:13 -0500

    readme sections added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Work Day Scheduler Starter Code
+
+## Application Description
+
+## Deployment
+
+## Installation
+
+## Usage
+
+## Credits
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var currentHr = parseInt(moment().format('H'));
 var classes = [".8AM", ".9AM", ".10AM", ".11AM", ".12PM", ".1PM", ".2PM", ".3PM", ".4PM", ".5PM", ".6PM"]
-var time = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+var hour = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 var allNotes = ["", "", "", "", "", "", "", "", "", "", ""]
 var classIndex = time.indexOf(hourNow);
 $("#currentDay").text(moment().format('dddd, MMMM D, YYYY'));
